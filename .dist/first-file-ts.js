fa0eeb252ba9507ec294ff0d602dae870caf6d82
marianagsoares <marianasoares.ti@gmail.com>
2023-03-15 12:22:55 -0300

    feat: add essential dependencies

---
+++ .dist/first-file-ts.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('O primeiro aqrquivo em typescript está sendo executado');
+const num = 5;
+const num2 = 10;
+const sumResult = num + num2;
+const text = 'O resultado é: ';
+console.log(`${text} ${sumResult}`);
---
+++ second-week/first-file-ts.ts
@@ -0,0 +1,9 @@
+console.log('O primeiro aqrquivo em typescript está sendo executado');
+
+const num: number = 5;
+const num2: number = 10;
+const sumResult: number = num + num2;
+
+const text: string = 'O resultado é: ';
+
+console.log(`${text} ${sumResult}`);
