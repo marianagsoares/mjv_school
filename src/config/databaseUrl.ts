b5d806ba2b6114cffe3e2924f6e917496942429e
marianagsoares <marianasoares.ti@gmail.com>
2023-04-06 21:35:31 -0300

    feat: add connection with database

---
+++ src/config/databaseUrl.ts
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/schoolNode';
+export default mongoose.connect(databaseUrl);
