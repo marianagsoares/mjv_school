76a6ad789751cfe8836ded2a8f345457eaff1262
marianagsoares <marianasoares.ti@gmail.com>
2023-04-06 21:17:23 -0300

    refactor: folder: structure

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './third-week/routers/index';
+import routes from './routers/index';
 
 const app = express();
 app.use(express.json());
